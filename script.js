5837b2332eb3933824e003b3ff78404e887a10b3
asboyy <muhammadfahmiputra2005@gmail.com>
2026-02-24 01:07:32 +0700

    update commit

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
+let tasks = [];
+
 const inputTask = document.getElementById("inputTask");
 const btnAdd = document.getElementById("btnAdd");
 const listTask = document.getElementById("listTask");
 
-btnAdd.addEventListener("click", function () {
-  const taskText = inputTask.value;
-
-  if (taskText === "") {
-    alert("Tidak Boleh Kosong!");
-    return;
-  }
-
+function addTaskToDOM(taskText) {
   const li = document.createElement("li");
   li.textContent = taskText;
+
   li.addEventListener("click", function () {
     li.classList.toggle("completed");
   });
@@ -21,11 +17,33 @@ btnAdd.addEventListener("click", function () {
 
   btnDelete.addEventListener("click", function (event) {
     event.stopPropagation();
+
+    //hapus dari array
+    tasks = tasks.filter(function (task) {
+      return task !== taskText;
+    });
+    //update localStorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    //Hapus dari DOM
     li.remove();
   });
 
   li.appendChild(btnDelete);
   listTask.appendChild(li);
+}
+
+btnAdd.addEventListener("click", function () {
+  const taskText = inputTask.value;
+
+  if (taskText === "") {
+    alert("Tidak Boleh Kosong!");
+    return;
+  }
+
+  tasks.push(taskText);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  addTaskToDOM(taskText);
 
   inputTask.value = "";
 });
@@ -33,5 +51,17 @@ btnAdd.addEventListener("click", function () {
 inputTask.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     btnAdd.click();
+  }
+});
+
+window.addEventListener("load", function () {
+  const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+
+  if (savedTasks) {
+    tasks = savedTasks;
+
+    tasks.forEach(function (task) {
+      addTaskToDOM(task);
+    });
   }
 });
